a2667891e0b4678cefa199080cc5831aedf30bbe
Sergio Sánchez <serjsa385@gmail.com>
2021-12-03 22:33:12 +0100

    Player class completed. Damage added
    Now the players can damage each others shooting.
    
    Code optimized and encapsulated
    
    other minor changes

--- src/js/Player.js
+++ src/js/Player.js
@@ -1,26 +1,50 @@
 class Player extends Phaser.GameObjects.Sprite
 {
-    constructor(scene, _speed, _lives, x, y, idx) {
-        super(scene, x, y, "dude");
+    constructor(scene, _speed, _lives, x, y, sprite, _idx) {
+        super(scene, x, y, sprite);
 
         this.speed = _speed;
         this.lives = _lives;
-        this.idx = idx;
+        this.idx = _idx;
         
+        this.alive = true;
         this.canBeDamaged = true;
 
+        // Pointer sprite for bullets arrow marker
+        this.arrow = scene.physics.add.staticSprite(400,300, 'sprite').setScale(0.15);
+
         // Add to scene
-        scene.add.existing(this);
-
-        scene.physics.world.enableBody(this);
-
-        //this.body.bo
+        this.scene.add.existing(this);
 
         // Add to players group
-        scene.players.add(this);        
+        this.scene.players.add(this);        
     }
 
+
     movement1()
+    {
+        if (a_key.isDown)
+        {
+            this.body.setVelocityX(-this.speed);
+            this.anims.play('left2', true);
+        }
+        else if (d_key.isDown)
+        {
+            this.body.setVelocityX(this.speed);
+            this.anims.play('right2', true);
+        }
+        else
+        {
+            this.body.setVelocityX(0);
+            this.anims.play('turn2');
+        }
+
+        if (w_key.isDown && this.body.touching.down)
+        {
+            this.body.setVelocityY(-330);
+        }
+    }
+    movement2()
     {
         // Movement
         if (cursors.left.isDown)
@@ -48,80 +72,126 @@ class Player extends Phaser.GameObjects.Sprite
         return this;
     }
 
-    movement2()
-    {
-        if (a_key.isDown)
-        {
-            this.body.setVelocityX(-this.speed);
-            this.anims.play('left2', true);
-        }
-        else if (d_key.isDown)
-        {
-            this.body.setVelocityX(this.speed);
-            this.anims.play('right2', true);
-        }
-        else
-        {
-            this.body.setVelocityX(0);
-            this.anims.play('turn2');
-        }
-
-        if (w_key.isDown && player2.body.touching.down)
-        {
-            this.body.setVelocityY(-330);
-        }
-    }
-
-    shooting()
+    shooting1()
     {
         // update arrow position
-        spr.x = this.x;
-        spr.y = this.y;
+        this.arrow.x = this.x;
+        this.arrow.y = this.y;
 
         // rotate arrow
         if (e_key.isDown)
         {
-            spr.angle += 2;
+            this.arrow.angle += 2;
         }
 
         if (q_key.isDown)
         {
-            spr.angle -= 2;
+            this.arrow.angle -= 2;
         }
 
         // shoot bullets
         if (Phaser.Input.Keyboard.JustDown(space_key))
         {
             // create bullet
-            var bullet = this.scene.physics.add.image(spr.x, spr.y, 'bullet').setImmovable(true);
+            var bullet = this.scene.physics.add.image(this.arrow.x, this.arrow.y, 'bullet').setImmovable(true); // Creates a bullet on the scene
             bullet.body.setAllowGravity(false);
 
-            this.scene.bullets.add(bullet); // add bullet to group
+            this.scene.bullets.add(bullet); // adds bullet to scene group
 
-            // set sprite rotation
-            bullet.angle = spr.angle;
-            //console.log(bullet.angle); // just to see the rotation
+            // set bullet's sprite rotation to match the arrow rotation
+            bullet.angle = this.arrow.angle;
 
             var bulletSpeed = 500;
             var vec = this.scene.physics.velocityFromAngle(bullet.angle, bulletSpeed); // angle velocity of arrow
 
-            // set bullet velocity
-            bullet.body.velocity.x = vec.y; // the real angle doesn't match what we can see on the sprite (90º is actually 0º)
+            // Sets bullet velocity
+            bullet.body.velocity.x = vec.y;
+            bullet.body.velocity.y = -vec.x;
+        }
+    }
+    shooting2()
+    {
+        // update arrow position
+        this.arrow.x = this.x;
+        this.arrow.y = this.y;
+
+        // rotate arrow
+        if (numpad_9_key.isDown)
+        {
+            this.arrow.angle += 2;
+        }
+
+        if (numpad_7_key.isDown)
+        {
+            this.arrow.angle -= 2;
+        }
+
+        // shoot bullets
+        if (Phaser.Input.Keyboard.JustDown(numpad_0_key))
+        {
+            // create bullet
+            var bullet = this.scene.physics.add.image(this.arrow.x, this.arrow.y, 'bullet').setImmovable(true); // Creates a bullet on the scene
+            bullet.body.setAllowGravity(false);
+
+            this.scene.bullets.add(bullet); // adds bullet to scene group
+
+            // set bullet's sprite rotation to match the arrow rotation
+            bullet.angle = this.arrow.angle;
+
+            var bulletSpeed = 500;
+            var vec = this.scene.physics.velocityFromAngle(bullet.angle, bulletSpeed); // angle velocity of arrow
+
+            // Sets bullet velocity
+            bullet.body.velocity.x = vec.y;
             bullet.body.velocity.y = -vec.x;
         }
     }
 
+    takeDamage()
+    {
+        this.lives--;
+
+        if(this.lives > 0) // If has enough lives to survive, takes damage
+        {
+            this.canBeDamaged = false;
+            console.log("Damaged player" + this.idx + ", " + this.lives + " lives left");
+            return true;
+        }
+        else // If not, dies :-(
+        {
+            this.alive = false;
+            console.log("Player" + this.idx + " dies");
+
+            this.arrow.destroy();   // Destroys the pointer
+            this.destroy();         // Destroys the player itself
+            return false;
+        }
+        
+    }
+
+    makeVulnerable()
+    {
+        this.canBeDamaged = true;
+        console.log("Player" + this.idx + " vulnerable again");
+    }
+
     update(){
 
-        // Checks for the controls
-        if(this.idx == 1)
-            this.movement1();
-        else if(this.idx == 2)
-            this.movement2();
+        if(this.alive) // If the player is alive
+        {
+            // Checks for the controls of each player based on the index
+            if(this.idx == 1)
+            {
+                this.movement1();
+                this.shooting1();
+            }  
+            else if(this.idx == 2)
+            {
+                this.movement2();
+                this.shooting2()
+            }
+        }
         
-        
-        this.shooting();
-
 
     }
 
--- src/js/script.js
+++ src/js/script.js
@@ -39,7 +39,8 @@ function preload () // load assets
 function create ()
 {
     // the timer is just a test for the 5 minute clock
-    var timer = this.time.delayedCall(5000, outOfTime, null, this);  // delay in ms
+    var secondsToEnd = 60;
+    var timer = this.time.delayedCall(secondsToEnd * 1000, outOfTime, null, this);  // delay in ms
     
     // objects in order from farther to nearest on screen
     this.add.image(0, 0, 'sky').setOrigin(0,0); // by default elements are positioned based on their center. Change so it matches the origin of the screen
@@ -50,14 +51,11 @@ function create ()
     platforms.create(600,400, 'ground');
 
 
-    /* 
-        PLAYERS
-    */
-
-    // Creates a group for the players
+    // Creates a group for the players & for the bullets
     this.players = this.add.group();
+    this.bullets = this.physics.add.group({inmovable: true, allowGravity:false});	
 
-    // Create player 1
+    // Player 1 animations
     this.anims.create({
         key: 'left1',
         frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
@@ -79,7 +77,7 @@ function create ()
     });
 
     
-    // Create player 2
+    // Player 2 animations
     this.anims.create({
         key: 'left2',
         frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
@@ -101,35 +99,44 @@ function create ()
     });
     
 
-    // Creates a group to store the bullets
-    bullets = this.physics.add.group({inmovable: true, allowGravity:false});	
-	spr = this.physics.add.staticSprite(400,300, 'sprite').setScale(0.25); //directional sprite arrow for bullets
-
     // Initializes the players
-    player1 = new Player(this, 160, 3, 0, 0, 1); //scene, speed, lives, x, y, index
-    player2 = new Player(this, 160, 3, 0, 0, 2);
+    player1 = this.physics.add.existing(new Player(this, 160, 3, 200, 100, "dude", 1)) //scene, speed, lives, x, y, sprite, index
+    player2 = this.physics.add.existing(new Player(this, 100, 3, 500, 100, "dude", 2))
+
+    player1.setScale(1.5); // Increases the scale cause they're tiny uwu
+    player2.setScale(1.5);
 
 
     // Adds colliders
-    this.physics.add.collider(player1, player2); // collider between characters
-    this.physics.add.collider(player1, platforms); // collider between character 1 and platforms
-    this.physics.add.collider(player2, platforms) // collider between character 2 and platforms
-    bulletsCollider = this.physics.add.collider(player1, bullets, hitBullets, null, this); // collider between player and bullets
-    this.physics.add.collider(platforms, bullets, killBullets, null, this); // collider between bullets and platforms
+        // Characters
+    this.physics.add.collider(this.players, this.players); // collider between characters
+    this.physics.add.collider(this.players, platforms); // collider between character 1 and platforms
+    
+        // Bullets
+    this.physics.add.collider(this.players, this.bullets, bulletOnPlayer, null, this); // collider between player and bullets
+    this.physics.add.collider(platforms, this.bullets, bulletOnWall, null, this); // collider between bullets and platforms
 
 
-    // player 1 input
+    // Player 1 inputs
+        // Mooving
     w_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
     a_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     d_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
-    // player 2 input
-    cursors = this.input.keyboard.createCursorKeys();
-
-    // other inputs ill sort them someday
+        // Shooting
     space_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     q_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
     e_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
+
+
+    // Player 2 inputs
+        // Mooving
+    cursors = this.input.keyboard.createCursorKeys();
+
+        //Shooting
+    numpad_0_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.NUMPAD_ZERO);
+    numpad_7_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.NUMPAD_SEVEN);
+    numpad_9_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.NUMPAD_NINE);
 
 }
 
@@ -138,8 +145,6 @@ function update ()
 {
     player1.update();
     player2.update();
-    
-        
 }
 
 
@@ -149,27 +154,30 @@ function outOfTime() // i was playing with this for the 5 minute timeout
 }
 
 
-function hitBullets(player, bullet) 
-{
-    if(player.canBeDamaged){ // if the player can be hit
-        bullet.disableBody(true,true); // destroy bullet when it hits the player
-        player.canBeDamaged = false; // player is now invincible
-        
-        var timerHurt = this.time.delayedCall(5000, playerHurt, null, this);  // wait 5 seconds and then call function playerHurt
-        player.body.alpha = 0.5; // turn down sprite transparency to visually show inmunity
-        bulletsCollider.destroy(); // destroy the collider
-    }
-}
 
-function killBullets(platforms,bullet)
+// -- Bullet collisions --
+
+function bulletOnWall(platforms, bullet)
 {
     // this will need an explosion sprite to show how the bullet collided with the platform
-    bullet.disableBody(true,true); // destroy bullet when it hits a platform
+    bullet.disableBody(true, true); // destroy bullet when it hits a platform
 }
 
-function playerHurt(player)
+function bulletOnPlayer(player, bullet)
 {
-    player.canBeDamaged = true; // after 5 seconds the player can be hit again
-    player.body.alpha = 1; // turn up sprite transparency to visually show it can be hit again
-    bulletsCollider = this.physics.add.collider(player1, bullets, hitBullets, null, this); // restore collider
+    bullet.disableBody(true, true); // destroy bullet when it hits a player
+
+    if(player.canBeDamaged) // If the player can be damaged...
+    {
+        if(player.takeDamage()) // If the player takes the damage and doesn't dies...
+        {
+            player.alpha = 0.5;
+
+            setTimeout(function () {
+                player.makeVulnerable();
+                player.alpha = 1;
+            }, 5000);
+        }
+    }
+    
 }

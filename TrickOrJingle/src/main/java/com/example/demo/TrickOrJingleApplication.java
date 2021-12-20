c5dc61c442d57f080b6017909a42f27967d30d22
Sergio Sánchez <serjsa385@gmail.com>
2021-12-20 12:26:19 +0100

    Spring Tool Suite project created

---
+++ TrickOrJingle/src/main/java/com/example/demo/TrickOrJingleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrickOrJingleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrickOrJingleApplication.class, args);
+	}
+
+}

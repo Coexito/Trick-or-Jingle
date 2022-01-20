cb3c9edb58a2aab8235ce426774afb76856d678e
bdevainy <auro.g.r.14@gmail.com>
2022-01-20 18:19:42 +0100

    fixed weapons and post method

--- TrickOrJingle/src/main/resources/static/js/scenes/login.js
+++ TrickOrJingle/src/main/resources/static/js/scenes/login.js
@@ -22,7 +22,7 @@ export class Login extends Phaser.Scene {
    
     
   }
-  
+  /*
 	create() {
 	    this.add.image(640, 340, 'background_login');
 	    
@@ -74,24 +74,53 @@ export class Login extends Phaser.Scene {
 	    	}
 	    });
     
+	}*/
+	
+	create() {
+	    this.add.image(640, 340, 'background_login');
+	    
+	    // Creates variable for managing the textview
+	    this.userInput = this.add.dom(640, 360).createFromCache("form");
+		let name = this.userInput.getChildByName("name");
+		let password = this.userInput.getChildByName("password");
+		
+		let text = this.add.text(450,500, '').setScale(2);
+		
+		let change = false; // boolean to change scene (at first is set to false)
+	    
+	    
+	    this.startButton = this.add.sprite(650, 600, 'button');	
+	    this.startButton.setInteractive().on('pointerdown', () => {
+			if(name.value != "" && password.value != "")
+			{			     	
+				$.ajax({
+					type: "POST",
+					async: false,
+					headers: {
+						'Accept': 'application/json',
+						'Content-type': 'application/json'
+					},
+					url: this.url + "users",
+					data: JSON.stringify({ nick: "" + name.value, password: "" + password.value }),
+					dataType: "json",
+					success : function (boolean) { // returned variable to check if we can change the scene
+							change = boolean;
+						}
+				})
+		     	
+		     	// Starts the next scene
+		     	if(change){ // if we access with an existing user and correct password or create a new one we can change the scene
+		     		this.scene.stop();
+	        		this.scene.start('Queue', { username: name.value, url: this.url});	
+				} else { // if the given password doesn't match the one of the existing user, we can't change the scene
+					text.setText('Wrong password. Try again'); // 
+				}
+	    	}
+	    });
+	    
+	    
+    	
 	}
   
-}
-
-
-
-
-function createUser(user, pass)
-{
-	$.ajax({
-		type: "POST",
-		headers: {
-			'Accept': 'application/json',
-			'Content-type' : 'application/json'	
-		},
-		url: this.url + "users",
-		data: JSON.stringify( { nick: ""+user, password: ""+pass } ),
-		dataType: "json" 
-	})
 }
 
